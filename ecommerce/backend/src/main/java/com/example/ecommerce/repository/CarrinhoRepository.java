59f702a1c4f2822d667dc894fce420e338e832dc
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-04-28 21:06:43 -0300

    Update CarrinhoRepository.java

--- ecommerce/backend/src/main/java/com/example/ecommerce/repository/CarrinhoRepository.java
+++ ecommerce/backend/src/main/java/com/example/ecommerce/repository/CarrinhoRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.ecommerce.model.Carrinho;
 import com.example.ecommerce.model.Produto;
+import com.example.ecommerce.model.Usuario;
 
 @Repository
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer> {
     Carrinho findByProduto(Produto produto);
+    Carrinho findByUsuario(Usuario usuario);
 }
+
+
