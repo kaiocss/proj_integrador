a51b9faa0fb8f658c2e05555ee74f36791cadcad
Vinicius Iago <160291470+VIONE34-PNG@users.noreply.github.com>
2025-05-05 15:37:49 -0300

    Update Pagamento.java

--- ecommerce/backend/src/main/java/com/example/ecommerce/model/Pagamento.java
+++ ecommerce/backend/src/main/java/com/example/ecommerce/model/Pagamento.java
@@ -1,26 +1,32 @@
 package com.example.ecommerce.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tipo_pagamento")
 public abstract class Pagamento {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
     private StatusPagamento status;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
 
     public Long getId() {
         return id;
@@ -38,6 +44,11 @@ public abstract class Pagamento {
         this.status = status;
     }
 
-    
+    public Pedido getPedido() {
+        return pedido;
+    }
 
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 }
