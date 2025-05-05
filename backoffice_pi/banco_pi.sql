f7885486fb6c739dafa7753e9e9b28f10e270bc5
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-05-05 20:20:45 -0300

    Update banco_pi.sql
    Adicionado funções do resumo

--- backoffice_pi/banco_pi.sql
+++ backoffice_pi/banco_pi.sql
@@ -9,24 +9,6 @@ status ENUM('ativado', 'desativado') NOT NULL,
 nome VARCHAR(100),
 cpf char(11)
 );
-INSERT INTO userBackoffice (email, senha, tipoUser) VALUES
-('adm@exemplo.com', 'senha123', 'adm');
-
-INSERT INTO userBackoffice (email, senha, tipoUser, status) VALUES 
-('teste@teste.com', 'senha123', 'adm', 'ativado');
-
-
-
-UPDATE userBackoffice SET senha = 'senha123' WHERE email = 'teste@teste.com';
-SELECT senha FROM userBackoffice WHERE email = 'teste@teste.com';
-UPDATE userBackoffice SET senha = '55a5e9e78207b4df8699d60886fa070079463547b095d1a05bc719bb4e6cd251' WHERE email = 'teste@teste.com';
-
-INSERT INTO userBackoffice (email, senha, tipoUser, status) VALUES 
-('estoque@teste.com', 'senha321', 'estoquista', 'ativado');
-UPDATE userBackoffice SET senha = '2288821c6b799cf47a8c9aa231f361ffb906bbee0d5fb5e1767509e27442cc62' WHERE email = 'estoque@teste.com';
-INSERT INTO userBackoffice (email, senha, tipoUser, status) VALUES 
-('cliente@teste.com', 'senha321', 'cliente', 'ativado');
-UPDATE userBackoffice SET senha = '2288821c6b799cf47a8c9aa231f361ffb906bbee0d5fb5e1767509e27442cc62' WHERE email = 'cliente@teste.com';
 
 CREATE TABLE IF NOT EXISTS produtos(
 codigo INT auto_increment PRIMARY KEY,
@@ -98,7 +80,6 @@ CREATE TABLE enderecos (
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
-
 ALTER TABLE CarrinhoProduto ADD COLUMN id_usuario BIGINT;
 
 ALTER TABLE CarrinhoProduto ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id);
@@ -110,6 +91,7 @@ CREATE TABLE pedido (
     data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(20) DEFAULT 'pendente', 
     total DECIMAL(10,2) NOT NULL,
+    numero_pedido VARCHAR(50) UNIQUE,
 
     CONSTRAINT fk_pedido_usuario FOREIGN KEY (usuario_id)
         REFERENCES usuarios(id) ON DELETE CASCADE,
@@ -117,6 +99,7 @@ CREATE TABLE pedido (
     CONSTRAINT fk_pedido_pagamento FOREIGN KEY (pagamento_id)
         REFERENCES pagamento(id) ON DELETE SET NULL  
 );
+
 
 CREATE TABLE item_pedido (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -131,6 +114,7 @@ CREATE TABLE item_pedido (
     CONSTRAINT fk_item_produto FOREIGN KEY (produto_codigo)
         REFERENCES produtos(codigo) ON DELETE CASCADE
 );
+
 
 CREATE TABLE pagamento (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -152,6 +136,8 @@ CREATE TABLE pagamento (
     CONSTRAINT fk_pagamento_pedido FOREIGN KEY (pedido_id)
         REFERENCES pedido(id) ON DELETE CASCADE
 );
+
+
 
 
 
