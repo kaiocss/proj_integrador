1b1366d90f842ce2da555df8a1441480f09b7313
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-06-02 18:54:43 -0300

    Create Pedido.java

---
+++ backoffice_pi/src/main/java/com/example/models/Pedido.java
@@ -0,0 +1,27 @@
+package com.example.models;
+
+public class Pedido {
+    private int id;
+    private String clienteNome;
+    private String status;
+    private String numeroPedido;
+    private double totalGeral;
+
+    public Pedido(int id, String clienteNome, String status, String numeroPedido, double total) {
+        this.id = id;
+        this.clienteNome = clienteNome;
+        this.status = status;
+        this.numeroPedido = numeroPedido;
+        this.totalGeral = totalGeral;
+    }
+
+    public int getId() { return id; }
+    public String getClienteNome() { return clienteNome; }
+    public String getStatus() { return status; }
+    public String getNumeroPedido() { return numeroPedido; }
+    public double getTotal() { return totalGeral; }
+
+    public void setStatus(String status) { this.status = status; }
+    public void setNumeroPedido(String numeroPedido) { this.numeroPedido = numeroPedido; }Add commentMore actions
+    public void setTotal(double total) { this.totalGeral = totalGeral; }
+}
